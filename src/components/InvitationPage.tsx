80119beb16cab3d8e78a45fc4910a7612cff13c9
VinuraGallage <th3-s7r4ng3r@users.noreply.github.com>
2023-12-07 20:40:01 +0530

    Fix: Bug from deploying the code

--- src/components/InvitationPage.tsx
+++ src/components/InvitationPage.tsx
@@ -1,5 +1,5 @@
 import "../css/Invitation.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // declaring the invitation component
 const InvitationPage = () => {
